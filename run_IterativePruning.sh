5fbc305a5dda9c755abb5a5e145f3a27a430c3e0
ohyoonju <ohyoonju7@gmail.com>
2021-07-15 22:30:59 +0900

    change hyperparmeter

--- run_IterativePruning.sh
+++ run_IterativePruning.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0,1 python3 IterativePruning.py --weights weights/yolov5_0430.pt --data recycle.yaml --img 1280 --batch-size 8 --crop_aug
+CUDA_VISIBLE_DEVICES=0,1 python3 IterativePruning.py --weights weights/yolov5_0430.pt --data recycle.yaml --img 1280 --batch-size 16 --crop_aug
 # data 파일 수정
